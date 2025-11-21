3adcfa0e1f83eff10b502fd75ae3e88a955f8aae
Yifan He <ivanhe.he@mail.utoronto.ca>
2025-11-21 15:40:22 -0500

    completed 1.1, 1.2

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,12 +1,20 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private final int audience;
+
+    public String getPlayID() {
+        return playID;
+    }
+
+    public int getAudience() {
+        return audience;
+    }
 
     public Performance(String playID, int audience) {
         this.playID = playID;
